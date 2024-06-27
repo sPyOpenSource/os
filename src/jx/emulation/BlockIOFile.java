a830bdb40333d48fac1e7aa29999b19dc8fa2bf5
X. Wang <xuyi@tetra.nl>
2024-06-27 21:23:32 +0200

    save

--- src/jx/emulation/BlockIOFile.java
+++ src/jx/emulation/BlockIOFile.java
@@ -5,30 +5,30 @@ import java.io.*;
 import jx.devices.bio.BlockIO;
 
 class BlockIOFile implements BlockIO {
-    RandomAccessFile file;
+    File file;
     final static int SECTOR_SIZE = 512;
     byte[] b = new byte[SECTOR_SIZE];
     public BlockIOFile(String filename, int sectors) {
-	try {
+	//try {
 	    //File f = new File(filename);
 	    //f.
-	    file = new RandomAccessFile(filename, "rw");
-	    file.seek(SECTOR_SIZE * (sectors-1));
-	    file.write(b);
-	} catch(IOException e) {
+	    file = new File(filename, "rw");
+	    //file.seek(SECTOR_SIZE * (sectors-1));
+	    //file.write(b);
+	/*} catch(IOException e) {
 	    e.printStackTrace();
 	    throw new Error();
-	}
+	}*/
     }
 
     @Override
     public int getCapacity() { 
-	try {
+	//try {
 	    return (int)(file.length()/SECTOR_SIZE);
-	} catch(IOException e) {
+	/*} catch(IOException e) {
 	    e.printStackTrace();
 	    throw new Error();
-	}
+	}*/
     }
 
     @Override
@@ -37,9 +37,9 @@ class BlockIOFile implements BlockIO {
     @Override
     public void readSectors(int startSector, int numberOfSectors, Memory buf, boolean synchronous) {
 	try {
-            file.seek(SECTOR_SIZE * startSector);
+            //file.seek(SECTOR_SIZE * startSector);
             for(int i = 0; i < numberOfSectors; i++) {
-                file.read(b);
+                //file.read(b);
                 buf.copyFromByteArray(b, 0, i * SECTOR_SIZE, SECTOR_SIZE);
             }
 	} catch(Exception e) {
@@ -50,10 +50,10 @@ class BlockIOFile implements BlockIO {
     @Override
     public void writeSectors(int startSector, int numberOfSectors, Memory buf, boolean synchronous) {
 	try {
-            file.seek(SECTOR_SIZE * startSector);
+            //file.seek(SECTOR_SIZE * startSector);
             for(int i = 0; i < numberOfSectors; i++) {
                 buf.copyToByteArray(b, 0,  i * SECTOR_SIZE, SECTOR_SIZE);
-                file.write(b);
+                //file.write(b);
             }
 	} catch(Exception e) {
 	    throw new Error();
